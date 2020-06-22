afaae18643decfe5486f245f6a4b2d14a6119f17
Seth Shikora <seth@shikora.org>
2020-06-22 07:47:07 -0700

    add single date picker as well

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import './App.css';
 import 'react-dates/initialize';
 import 'react-dates/lib/css/_datepicker.css';
 
-import { DateRangePicker } from 'react-dates';
+import { DateRangePicker, SingleDatePicker } from 'react-dates';
+
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      date: null,
+      focused: null,
       startDate: null,
       endDate: null,
       focusedInput: null,
@@ -19,15 +22,28 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <DateRangePicker
-          startDateId="startDate"
-          endDateId="endDate"
-          startDate={this.state.startDate}
-          endDate={this.state.endDate}
-          onDatesChange={({ startDate, endDate }) => { this.setState({ startDate, endDate })}}
-          focusedInput={this.state.focusedInput}
-          onFocusChange={(focusedInput) => { this.setState({ focusedInput })}}
-        />
+        <div>
+          <DateRangePicker
+            startDateId="startDate"
+            endDateId="endDate"
+            startDate={this.state.startDate}
+            endDate={this.state.endDate}
+            onDatesChange={({ startDate, endDate }) => { this.setState({ startDate, endDate })}}
+            focusedInput={this.state.focusedInput}
+            onFocusChange={(focusedInput) => { this.setState({ focusedInput })}}
+          />
+        </div>
+        <br/>
+        <br/>
+        <div>
+          <SingleDatePicker
+            date={this.state.date}
+            onDateChange={date => this.setState({ date })}
+            focused={this.state.focused}
+            onFocusChange={({ focused }) => this.setState({ focused })}
+            id="this_is_an_id"
+          />
+        </div>
       </div>
     );
   }
